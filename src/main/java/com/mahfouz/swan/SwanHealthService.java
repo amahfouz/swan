8cd4c3b9f9b985d371f2856c09b4a883c172865c
Ayman Mahfouz <amahfouz@gmail.com>
2017-03-25 10:41:31 -0700

    Added generic sample GET/PUT handlers with URL parsing.

--- src/main/java/com/mahfouz/swan/SwanHealthService.java
+++ src/main/java/com/mahfouz/swan/SwanHealthService.java
@@ -1,32 +1,64 @@
 package com.mahfouz.swan;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import org.springframework.web.servlet.HandlerMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.HandlerMapping;
+
 @RestController
-public final class SwanHealthService {
-    
+public class SwanHealthService {
+
+    private static final Log log
+        = LogFactory.getLog(SwanHealthService.class.getName());
+
     @RequestMapping(value="/**", method=RequestMethod.POST)
-    public String save(HttpServletRequest request) {
-         String restOfTheUrl = (String) request.getAttribute
-            (HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
-        return "PUT success:" + restOfTheUrl;
+    @ResponseStatus(HttpStatus.CREATED)
+    public @ResponseBody String save(HttpServletRequest request) {
+
+         log.info("Entered save method.");
+
+         String typeName = trimUrlToEntityName(request);
+
+        return "PUT success:" + typeName;
+
     }
 
     @RequestMapping(value="/**", method=RequestMethod.GET)
-    public String get(HttpServletRequest request) {
-         String restOfTheUrl = (String) request.getAttribute
-            (HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
-        return "GET success:" + restOfTheUrl;
-    }    
+    public String load(HttpServletRequest request) {
 
-    @RequestMapping("/health")
-    public SwanHealth health() {
-        return new SwanHealth("UP", 123);
+        log.info("Entered load method.");
+
+        String typeName = trimUrlToEntityName(request);
+
+        return "GET success:" + typeName;
+    }
+
+    private String trimUrlToEntityName(HttpServletRequest request) {
+        String restOfTheUrl = (String) request.getAttribute
+            (HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+
+        if (restOfTheUrl.startsWith("/"))
+            restOfTheUrl = restOfTheUrl.substring(1);
+
+        if (restOfTheUrl.contains("/"))
+            throw new UnrecognizedUrlException();
+
+        return restOfTheUrl;
+    }
+
+    //
+    // Nested
+    //
+
+    @ResponseStatus(value=HttpStatus.NOT_FOUND, reason="URL unrecognized.")
+    public final class UnrecognizedUrlException extends RuntimeException {
+
     }
 }
